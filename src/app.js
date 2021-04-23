838224dbe79e024d7c1af7414c33de97dbcb5e80
edup94 <eduardo.paredes@estudiantes.utec.edu.uy>
2021-04-23 12:18:58 +0000

    guardado2

--- src/app.js
+++ src/app.js
@@ -6,13 +6,14 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = () => {
-  document.querySelector(".container").addEventListener("click", () => {
-    document.querySelector("#newCard").innerHTML = generateCard();
+  document.querySelector(".btn").addEventListener("click", () => {
+    document.querySelector(".card").innerHTML = generateCard();
+    // document.querySelector(".card").classList.add(generateSymbol());
+    // document.querySelector(".btn").innerHTML = generateCard();
   });
 };
 
 let generateCard = () => {
-  let figure = ["♥", "♦", "♣", "♠"];
   let character = [
     "A",
     "2",
@@ -28,23 +29,12 @@ let generateCard = () => {
     "Q",
     "K"
   ];
-
-  let figureIndex = Math.floor(Math.random() * figure.length);
   let characterIndex = Math.floor(Math.random() * character.length);
-  let data = document.querySelectorAll(".figura1");
-  let data1 = document.querySelectorAll(".numero");
-  let data2 = document.querySelectorAll(".figura2");
+  return character[characterIndex];
+};
 
-  for (let i = 0; i < data.length; i++) {
-    data[i].innerHTML = `
-            ${figure[figureIndex]}`;
-    data1[i].innerHTML = `
-            ${character[characterIndex]}`;
-    data2[i].innerHTML = `
-            ${figure[figureIndex]}`;
-  }
-  if (figure[figureIndex] == "♦" || figure[figureIndex] == "♥") {
-    document.querySelector(".figura1").style.color = "red";
-    document.querySelector(".figura2").style.color = "red";
-  }
+let generateSymbol = () => {
+  let figure = ["hearts", "diams", "clubs", "spades"];
+  let figureIndex = Math.floor(Math.random() * figure.length);
+  return figure[figureIndex];
 };
