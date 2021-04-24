0892d6bd9e1bd6cf018c74e39f35b0aaf62c1f9b
edup94 <eduardo.paredes@estudiantes.utec.edu.uy>
2021-04-24 13:06:05 +0000

    guardado3

--- src/app.js
+++ src/app.js
@@ -5,14 +5,6 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = () => {
-  document.querySelector(".btn").addEventListener("click", () => {
-    document.querySelector(".card").innerHTML = generateCard();
-    // document.querySelector(".card").classList.add(generateSymbol());
-    // document.querySelector(".btn").innerHTML = generateCard();
-  });
-};
-
 let generateCard = () => {
   let character = [
     "A",
@@ -29,12 +21,28 @@ let generateCard = () => {
     "Q",
     "K"
   ];
-  let characterIndex = Math.floor(Math.random() * character.length);
-  return character[characterIndex];
+  return character[Math.floor(Math.random() * character.length)];
 };
 
 let generateSymbol = () => {
   let figure = ["hearts", "diams", "clubs", "spades"];
   let figureIndex = Math.floor(Math.random() * figure.length);
   return figure[figureIndex];
+};
+
+let symbol = generateSymbol();
+let number = generateCard();
+
+document.querySelector(".btn").addEventListener("click", () => {
+  document.querySelector(".card").classList.remove(`${symbol}`);
+  symbol = generateSymbol();
+  number = generateCard();
+
+  document.querySelector(".card").classList.add(`${symbol}`);
+  document.querySelector(".card").innerHTML = `${number}`;
+});
+
+window.onload = () => {
+  document.querySelector(".card").classList.add(symbol);
+  document.querySelector(".card").innerHTML = number;
 };
